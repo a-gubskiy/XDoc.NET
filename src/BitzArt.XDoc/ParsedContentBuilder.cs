bcc723fddae6b6b3b6bf8587591d91644535c079
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-02-25 22:17:03 +0200

    Update ParsedContentBuilder

--- src/BitzArt.XDoc/ParsedContentBuilder.cs
+++ src/BitzArt.XDoc/ParsedContentBuilder.cs
@@ -1,4 +1,6 @@
+using System.Collections.Frozen;
 using System.Collections.Immutable;
+using System.Reflection;
 using System.Xml;
 using System.Xml.Linq;
 
@@ -30,8 +32,26 @@ public class ParsedContentBuilder
     /// <param name="memberDocumentation">The member documentation containing XML nodes and member information.</param>
     /// <returns>A <see cref="ParsedContent"/> object containing resolved documentation, references and inheritance chain.</returns>
     /// <typeparam name="T">The type of the member being documented.</typeparam>
-    public ParsedContent Build<T>(MemberDocumentation<T> memberDocumentation) where T : class =>
-        GetParsedContent(memberDocumentation.Node, memberDocumentation.Source, memberDocumentation.DeclaringType);
+    public ParsedContent Build<T>(MemberDocumentation<T> memberDocumentation) where T : MemberInfo =>
+        GetParsedContent(memberDocumentation.Node, memberDocumentation.Source, memberDocumentation.Member);
+
+    public ParsedContent Build<T>(PropertyDocumentation memberDocumentation) where T : MemberInfo =>
+        GetParsedContent(memberDocumentation.Node, memberDocumentation.Source, memberDocumentation.Member);
+
+    private ParsedContent GetParsedContent<TMember>(XmlNode? xmlNode, XDoc xDoc, TMember memberInfo)
+        where TMember : MemberInfo
+    {
+        var parent = GetParent(xmlNode, xDoc, memberInfo);
+        var references = GetReferences(xmlNode, xDoc);
+
+        return new ParsedContent
+        {
+            Parent = parent,
+            References = references,
+            Xml = xmlNode,
+            Type = null
+        };
+    }
 
     private ParsedContent GetParsedContent(XmlNode? xmlNode, XDoc xDoc, Type type)
     {
@@ -111,5 +131,77 @@ public class ParsedContentBuilder
         var parent = GetParent(parentTypeDocumentation?.Node, xDoc, type.BaseType);
 
         return parent;
+    }
+
+    private ParsedContent? GetParent(XmlNode? xmlNode, XDoc xDoc, MemberInfo memberInfo)
+    {
+        if (xmlNode?.FirstChild?.Name == "inheritdoc")
+        {
+            var parentTypes = GetParentTypes(memberInfo);
+
+            foreach (var parent in parentTypes)
+            {
+                var parentMembers = parent.GetMember(memberInfo.Name);
+
+                foreach (MemberInfo parentMember in parentMembers)
+                {
+                    if (parentMember is PropertyInfo parentPropertyInfo)
+                    {
+                        var parentPropertyDocumentation = xDoc.Get(parentPropertyInfo);
+
+                        if (parentPropertyDocumentation == null)
+                        {
+                            continue;
+                        }
+
+                        var references = GetReferences(parentPropertyDocumentation.Node, xDoc);
+                        var parentMemberParent = GetParent(parentPropertyDocumentation.Node, xDoc,
+                            parentPropertyDocumentation.DeclaringType);
+
+                        return new ParsedContent
+                        {
+                            Type = parentPropertyDocumentation.DeclaringType,
+                            Xml = parentPropertyDocumentation.Node,
+                            References = references,
+                            Parent = parentMemberParent
+                        };
+                    }
+                    else if (parentMember is FieldInfo parentFieldInfo)
+                    {
+                        // xDoc.Get(parentFieldInfo);
+                    }
+                    else if (parentMember is MethodInfo parentMethodInfo)
+                    {
+                        // xDoc.Get(parentMethodInfo);
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Retrieves a collection of parent types (interfaces and declaring type) for a given member.
+    /// </summary>
+    /// <param name="memberInfo">The member information to analyze.</param>
+    /// <returns>A frozen set of <see cref="Type"/> objects representing parent types and interfaces.</returns>
+    public IReadOnlyCollection<Type> GetParentTypes(MemberInfo memberInfo)
+    {
+        var result = new List<Type>();
+
+        var interfaces = memberInfo.DeclaringType?.GetInterfaces() ?? [];
+
+        if (interfaces.Any())
+        {
+            result.AddRange(interfaces);
+        }
+
+        if (memberInfo.DeclaringType != null)
+        {
+            result.Add(memberInfo.DeclaringType);
+        }
+
+        return result.ToFrozenSet();
     }
 }
