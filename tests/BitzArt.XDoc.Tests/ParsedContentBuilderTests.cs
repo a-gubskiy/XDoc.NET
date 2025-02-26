85d59ba8de17db89eee05d68db9a493a5a3c3d76
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-02-26 23:56:35 +0200

    Add ParsedContentBuilderTests

---
+++ tests/BitzArt.XDoc.Tests/ParsedContentBuilderTests.cs
@@ -0,0 +1,95 @@
+using System.Xml;
+
+namespace BitzArt.XDoc.Tests;
+
+public class ParsedContentBuilderTests
+{
+    private readonly ParsedContentBuilder _builder;
+    private readonly XDoc _xDoc;
+
+    public ParsedContentBuilderTests()
+    {
+        _builder = new ParsedContentBuilder();
+        _xDoc = new XDoc();
+    }
+
+    [Fact]
+    public void Build_TypeWithoutReferences_ShouldReturnBasicParsedContent()
+    {
+        // Arrange
+        var typeDoc = new TypeDocumentation(
+            _xDoc, 
+            typeof(SimpleClass),
+            CreateXmlNode("<doc><summary>Simple class</summary></doc>"));
+
+        // Act
+        var result = _builder.Build(typeDoc);
+
+        // Assert
+        Assert.Equal("SimpleClass", result.Name);
+        Assert.Null(result.Parent);
+        Assert.Empty(result.DeclaredReferences);
+        Assert.NotNull(result.Xml);
+    }
+
+    [Fact]
+    public void Build_TypeWithInheritDoc_ShouldReturnParsedContentWithParent()
+    {
+        // Arrange
+        var typeDoc = new TypeDocumentation(_xDoc, typeof(DerivedClass), CreateXmlNode("<doc><inheritdoc/></doc>"));
+
+        // Act
+        var result = _builder.Build(typeDoc);
+
+        // Assert
+        Assert.Equal("DerivedClass", result.Name);
+        Assert.NotNull(result.Parent);
+    }
+
+    [Fact]
+    public void Build_TypeWithSeeReference_ShouldReturnParsedContentWithReferences()
+    {
+        // Arrange
+        var typeDoc = new TypeDocumentation(
+            _xDoc,
+            typeof(ClassWithReferences),
+            CreateXmlNode("<doc><summary>See <see cref=\"T:SimpleClass\"/></summary></doc>"));
+
+        // Act
+        var result = _builder.Build(typeDoc);
+
+        // Assert
+        Assert.NotEmpty(result.DeclaredReferences);
+        Assert.Equal("SimpleClass", result.DeclaredReferences.First());
+    }
+    
+    private XmlNode CreateXmlNode(string xml)
+    {
+        var doc = new XmlDocument();
+        doc.LoadXml(xml);
+        return doc.DocumentElement!;
+    }
+}
+
+// Test helper classes
+public class SimpleClass
+{
+}
+
+public class DerivedClass : SimpleClass
+{
+}
+
+public class ClassWithReferences
+{
+}
+
+public interface ITestInterface
+{
+    string Name { get; }
+}
+
+public class ImplementingClass : ITestInterface
+{
+    public string Name => "";
+}
