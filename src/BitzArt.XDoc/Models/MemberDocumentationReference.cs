32f66d8b5e3cd0e4dda8151248cb84aa20c421b3
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-03-04 22:31:55 +0200

    Update project structure

--- src/BitzArt.XDoc/Models/MemberDocumentationReference.cs
+++ src/BitzArt.XDoc/Models/MemberDocumentationReference.cs
@@ -18,18 +18,6 @@ public abstract class MemberDocumentationReference
 public class InheritanceMemberDocumentationReference : MemberDocumentationReference
 {
     public Type TargetType { get; internal set; }
-
-    // This class can then be used something like the following:
-    // private void ExampleUsage(MemberDocumentationReference someReference)
-    // {
-    //     // Let's assume we are currently iterating through a list of MemberDocumentationReferences or something
-    //
-    //     // this gives you knowledge that this reference was caused by an <inheritdoc> node
-    //     if (someReference is InheritanceMemberDocumentationReference inheritanceReference)
-    //     {
-    //         // do something specific to inheritance reference
-    //     }
-    // }
 }
 
 /// <summary>
