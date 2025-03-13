319811b1d4105883b60397de62d3c7028cc5f747
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-03-13 18:12:29 +0200

    Add test

--- tests/EntityFrameworkCore/BitzArt.XDoc.EntityFrameworkCore.Tests/EntityTypeBuilderExtensionsTests.cs
+++ tests/EntityFrameworkCore/BitzArt.XDoc.EntityFrameworkCore.Tests/EntityTypeBuilderExtensionsTests.cs
@@ -24,8 +24,6 @@ public class EntityTypeBuilderExtensionsTests
 
         var testContext = new TestDbContext1((context, modelBuilder) =>
         {
-            var xdoc = new XDoc();
-
             modelBuilder
                 .Entity<MyFirstClass>()
                 .HasPropertyComment(x => x.Id, testComment);
@@ -140,6 +138,35 @@ public class EntityTypeBuilderExtensionsTests
             .Model
             .FindEntityType(typeof(MyFirstClass))!
             .FindProperty(nameof(MyFirstClass.Id))!
+            .GetComment());
+    }
+
+    [Fact]
+    public void HasPropertyComment_WithPropertyNameStringForProperty_ShouldSetComment()
+    {
+        // Arrange
+        var propertyName = "MyCustomProperty";
+
+        var testContext = new TestDbContext6((context, modelBuilder) =>
+        {
+            var xdoc = new XDoc();
+
+            modelBuilder
+                .Entity<MyFirstClass>()
+                .Property<string>(propertyName)
+                .HasPropertyComment<MyFirstClass, string, MySecondClass, string?>(xdoc, propertyName,
+                    o => o.NullableName);
+        });
+
+        // Act
+        _ = testContext.Model;
+
+        // Assert
+        Assert.Equal(MySecondClass.NullableNameComment, testContext
+            .GetService<IDesignTimeModel>()
+            .Model
+            .FindEntityType(typeof(MyFirstClass))!
+            .FindProperty(propertyName)!
             .GetComment());
     }
 }
