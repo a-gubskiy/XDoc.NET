97102a1719a232c34a109c7fb2a7e4cb6cd2b01a
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-03-16 23:09:09 +0200

    Update test

--- tests/Render/BitzArt.XDoc.PlainText.Tests/PlainTextRendererTests.cs
+++ tests/Render/BitzArt.XDoc.PlainText.Tests/PlainTextRendererTests.cs
@@ -5,34 +5,37 @@ namespace BitzArt.XDoc.Tests;
 
 public class PlainTextRendererTests
 {
-    // [Fact]
-    // public void Render_PlainTextRenderer_ShouldReturnFormattedText()
-    // {
-    //     // Arrange
-    //     var xml = @"<member name=""P:MyCompany.Library.WeeklyMetrics.SuccessRatio"">
-    //                     <summary>
-    //                         The ratio of <see cref=""P:MyCompany.Library.WeeklyMetrics.Progress""/> to <see cref=""P:MyCompany.Library.WeeklyMetrics.Objective""/> for this <see cref=""T:MyCompany.Library.WeeklyMetric""/>.
-    //                     </summary>
-    //                 </member>";
-    //
-    //     var xmlNode = GetXmlNode(xml);
-    //     
-    //     // Act
-    //     var str = PlainTextRenderer.Render(xmlNode!);
-    //
-    //     // Assert
-    //     Assert.Contains("The ratio of Progress to Objective for this WeeklyMetric.", str);
-    // }
-    
-    
+    [Fact]
+    public void Render_PlainTextRenderer_ShouldReturnFormattedText()
+    {
+        // Arrange
+        var xml = @"<member name=""P:MyCompany.Library.WeeklyMetrics.SuccessRatio"">
+                        <summary>
+                            The ratio of <see cref=""P:MyCompany.Library.WeeklyMetrics.Progress""/> to <see cref=""P:MyCompany.Library.WeeklyMetrics.Objective""/> for this <see cref=""T:MyCompany.Library.WeeklyMetric""/>.
+                        </summary>
+                    </member>";
+
+        var xmlNode = GetXmlNode(xml);
+
+
+        // Act
+        var memberDocumentation = new TestPropertyDocumentation(xmlNode);
+
+        var str = PlainTextRenderer.Render(memberDocumentation);
+
+        // Assert
+        Assert.Contains("The ratio of Progress to Objective for this WeeklyMetric.", str);
+    }
+
+
     [Fact]
     public void RenderInheritedDocumentation_PlainTextRenderer_ShouldReturnFormattedText()
     {
-       // Act
+        // Act
         var source = new XDoc();
         var propertyDocumentation = source.Get(typeof(Dog).GetProperty(nameof(Dog.Property1)));
         var text = propertyDocumentation.ToPlainText();
-       
+
 
         // Assert
         Assert.Contains("Description of Property1", text);
@@ -46,5 +49,13 @@ public class PlainTextRendererTests
         XmlNode xmlNode = xmlDoc.DocumentElement!; // Get the node
 
         return xmlNode;
+    }
+}
+
+public class TestPropertyDocumentation : MemberDocumentation
+{
+    public TestPropertyDocumentation(XmlNode xmlNode)
+        : base(new XDoc(), xmlNode)
+    {
     }
 }
