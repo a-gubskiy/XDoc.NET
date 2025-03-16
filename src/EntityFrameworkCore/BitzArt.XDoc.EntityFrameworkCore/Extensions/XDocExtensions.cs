9662d56b9924727cd68a090b8cac22057795584e
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-03-16 13:34:59 +0200

    Update XDocExtensions

--- src/EntityFrameworkCore/BitzArt.XDoc.EntityFrameworkCore/Extensions/XDocExtensions.cs
+++ src/EntityFrameworkCore/BitzArt.XDoc.EntityFrameworkCore/Extensions/XDocExtensions.cs
@@ -2,20 +2,19 @@ using System.Linq.Expressions;
 
 namespace BitzArt.XDoc;
 
-public static class XDocHelper
+internal static class XDocExtensions
 {
     /// <summary>
     /// Extracts documentation comment from a property specified in an expression.
     /// </summary>
-    /// <typeparam name="TEntity">The entity type being configured</typeparam>
     /// <typeparam name="TCommentTargetEntity">The entity type containing the property whose documentation will be used</typeparam>
     /// <typeparam name="TCommentTargetProperty">The type of the property whose documentation will be used</typeparam>
     /// <param name="xdoc">The XDoc instance used to extract documentation</param>
     /// <param name="commentTargetPropertyExpression">An expression that identifies the property whose documentation will be used</param>
     /// <returns>The extracted documentation comment as plain text</returns>
     /// <exception cref="InvalidOperationException">Thrown when the property name cannot be determined or is empty</exception>
-    internal static string GetComment<TCommentTargetEntity, TCommentTargetProperty>(
-        XDoc xdoc,
+    internal static string Get<TCommentTargetEntity, TCommentTargetProperty>(
+        this XDoc xdoc,
         Expression<Func<TCommentTargetEntity, TCommentTargetProperty>> commentTargetPropertyExpression)
         where TCommentTargetEntity : class
     {
