ec8910d1912ed2b823006d1652b2ee82dbeec45e
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-03-18 23:36:20 +0200

    Update field

--- src/Render/BitzArt.XDoc.PlainText/SimpleDocumentationReference.cs
+++ src/Render/BitzArt.XDoc.PlainText/SimpleDocumentationReference.cs
@@ -16,18 +16,18 @@ public class SimpleDocumentationReference : DocumentationReference
     /// <summary>
     /// Gets or initializes a value indicating whether this documentation inherits from another element.
     /// </summary>
-    public bool IsInheritDoc { get; init; }
+    public bool IsInherit { get; init; }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="SimpleDocumentationReference"/> class.
     /// </summary>
     /// <param name="requirementNode">The XML node containing the documentation requirement.</param>
     /// <param name="cref">The code reference string identifying the target element.</param>
-    /// <param name="isInheritDoc">A value indicating whether this documentation inherits from another element.</param>
-    public SimpleDocumentationReference(XmlNode requirementNode, string cref, bool isInheritDoc)
+    /// <param name="isInherit">A value indicating whether this documentation inherits from another element.</param>
+    public SimpleDocumentationReference(XmlNode requirementNode, string cref, bool isInherit)
         : base(requirementNode, null)
     {
         Cref = cref;
-        IsInheritDoc = isInheritDoc;
+        IsInherit = isInherit;
     }
 }
