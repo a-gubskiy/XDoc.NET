6a28340141b84a57cad0a378dd5ba660aa24d31b
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-03-25 23:27:38 +0200

    Update tests

--- tests/Render/BitzArt.XDoc.PlainText.Tests/PlainTextRendererTests.cs
+++ tests/Render/BitzArt.XDoc.PlainText.Tests/PlainTextRendererTests.cs
@@ -2,8 +2,70 @@ using System.Xml;
 
 namespace BitzArt.XDoc.Tests;
 
+public class TestClass
+{
+}
+
 public class PlainTextRendererTests
 {
+    [Fact]
+    public void Render_ExtractsPropertySummary_FromDocumentation()
+    {
+        // Arrange
+        var assembly = GetType().Assembly;
+
+        List<TestMemberNode> nodes =
+        [
+            new TestMemberNode(typeof(TestClass), "none"),
+
+            new TestMemberNode(TestNodeType.Property,
+                "BitzArt.XDoc.Tests.BaseTestClass.Name",
+                "<member name=\"P:BitzArt.XDoc.Tests.TestClass.TestProperty\"><summary>Test property documentation.</summary></member>")
+        ];
+
+        var xml = nodes.GetXml(assembly);
+        var xmlDocument = new XmlDocument();
+        xmlDocument.LoadXml(xml);
+
+        var node = xmlDocument.SelectSingleNode("//member[@name='P:BitzArt.XDoc.Tests.TestClass.TestProperty']");
+
+        var memberDocumentation = new FakeMemberDocumentation(node);
+
+        var plainTextRenderer = new PlainTextRenderer(new XDoc());
+        var comment = plainTextRenderer.Render(memberDocumentation);
+
+        Assert.Equal("Test property documentation.", comment);
+    }
+
+    [Fact]
+    public void Render_ReturnsEmptyString_WhenInheritDocTagProvided()
+    {
+        // Arrange
+        var assembly = GetType().Assembly;
+
+
+        List<TestMemberNode> nodes =
+        [
+            new TestMemberNode(typeof(TestClass), "node with inherit doc test"),
+            new TestMemberNode(TestNodeType.Property,
+                "BitzArt.XDoc.Tests.TestClass.Name",
+                "<member name=\"P:BitzArt.XDoc.Tests.TestClass.Name\"><inheritdoc /></member>")
+        ];
+
+        var xml = nodes.GetXml(assembly);
+        var xmlDocument = new XmlDocument();
+        xmlDocument.LoadXml(xml);
+
+        var node = xmlDocument.SelectSingleNode("//inheritdoc");
+
+        var memberDocumentation = new FakeMemberDocumentation(node);
+
+        var plainTextRenderer = new PlainTextRenderer(new XDoc());
+        var comment = plainTextRenderer.Render(memberDocumentation);
+
+        Assert.Equal("", comment);
+    }
+
     [Fact]
     public void Render_ReturnsEmptyString_WhenDocumentationIsNull()
     {
@@ -20,9 +82,10 @@ public class PlainTextRendererTests
         // Arrange
         var textNode = new XmlDocument().CreateTextNode("Hello World");
         var memberDocumentation = new FakeMemberDocumentation(textNode);
+        var plainTextRenderer = new PlainTextRenderer(new XDoc());
 
         // Act
-        var result = memberDocumentation.ToPlainText();
+        var result = plainTextRenderer.Render(memberDocumentation);
 
         // Assert
         Assert.Equal("Hello World", result);
@@ -36,9 +99,10 @@ public class PlainTextRendererTests
         element.InnerXml = "Line1\n   Line2\nLine3";
 
         var memberDocumentation = new FakeMemberDocumentation(element);
+        var plainTextRenderer = new PlainTextRenderer(new XDoc());
 
         // Act
-        var result = memberDocumentation.ToPlainText();
+        var result = plainTextRenderer.Render(memberDocumentation);
 
         // Assert
         Assert.Equal("Line1 Line2 Line3", result);
@@ -47,22 +111,21 @@ public class PlainTextRendererTests
     [Fact]
     public void Render_RendersCrefReference_ReturnsShortTypeName()
     {
-        var xmlDocument = new XmlDocument();
-        XmlNode xmlNode = xmlDocument.CreateNode(XmlNodeType.Element, "test", null);
-
-
         // Arrange
+        var xmlDocument = new XmlDocument();
+        var xmlNode = xmlDocument.CreateNode(XmlNodeType.Element, "test", null);
         var element = xmlDocument.CreateElement("see");
+
         element.SetAttribute("cref", "T:BitzArt.XDoc.Models.SomeType");
         xmlNode.AppendChild(element);
 
         xmlDocument.AppendChild(xmlNode);
 
-
         var memberDocumentation = new FakeMemberDocumentation(xmlNode);
+        var plainTextRenderer = new PlainTextRenderer(new XDoc());
 
         // Act
-        var result = memberDocumentation.ToPlainText();
+        var result = plainTextRenderer.Render(memberDocumentation);
 
         // Assert
         Assert.Equal("SomeType", result);
@@ -71,22 +134,21 @@ public class PlainTextRendererTests
     [Fact]
     public void Render_RendersCrefReference_ReturnsMethodName()
     {
-        var xmlDocument = new XmlDocument();
-        XmlNode xmlNode = xmlDocument.CreateNode(XmlNodeType.Element, "test", null);
-
-
         // Arrange
+        var xmlDocument = new XmlDocument();
+        var xmlNode = xmlDocument.CreateNode(XmlNodeType.Element, "test", null);
         var element = xmlDocument.CreateElement("see");
+
         element.SetAttribute("cref", "M:BitzArt.XDoc.Models.SomeType.SomeMethod");
         xmlNode.AppendChild(element);
 
         xmlDocument.AppendChild(xmlNode);
 
-
         var memberDocumentation = new FakeMemberDocumentation(xmlNode);
+        var plainTextRenderer = new PlainTextRenderer(new XDoc());
 
         // Act
-        var result = memberDocumentation.ToPlainText();
+        var result = plainTextRenderer.Render(memberDocumentation);
 
         // Assert
         Assert.Equal("SomeType.SomeMethod", result);
@@ -98,9 +160,10 @@ public class PlainTextRendererTests
         // Arrange
         var element = new XmlDocument().CreateElement("empty");
         var memberDocumentation = new FakeMemberDocumentation(element);
+        var plainTextRenderer = new PlainTextRenderer(new XDoc());
 
         // Act
-        var result = memberDocumentation.ToPlainText();
+        var result = plainTextRenderer.Render(memberDocumentation);
 
         // Assert
         Assert.Equal(string.Empty, result);
