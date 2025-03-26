46814069bc726b2a424cafa251e859e7e30bb49c
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-03-27 00:31:36 +0200

    Update tests

--- tests/BitzArt.XDoc.Tests/Tests/TypeDocumentationTests.cs
+++ tests/BitzArt.XDoc.Tests/Tests/TypeDocumentationTests.cs
@@ -1,29 +1,27 @@
-using System.Reflection;
 using System.Xml;
 
 namespace BitzArt.XDoc.Tests;
 
-public class TypeDocumentationTestClass
-{
-    public int MyProperty { get; set; }
-}
-
-public class InheritedTypeDocumentationTestClass : TypeDocumentationTestClass
-{
-}
-
 public class TypeDocumentationTests
 {
+    public class TestClass
+    {
+        public int MyProperty { get; set; }
+    }
+
+    public class InheritedTestClass : TestClass
+    {
+    }
+    
     [Fact]
     public void GetDocumentation_PropertyInfo_ReturnsPropertyDocumentation()
     {
         var source = new XDoc();
-        
+
         var xmlDocument = new XmlDocument();
         var node = xmlDocument.CreateTextNode("blah");
-        var propertyInfo = typeof(TypeDocumentationTestClass).GetProperty(nameof(TypeDocumentationTestClass.MyProperty));
-
-        var typeDocumentation = new TypeDocumentation(source, typeof(TypeDocumentationTestClass), node);
+        var propertyInfo = typeof(TestClass).GetProperty(nameof(TestClass.MyProperty))!;
+        var typeDocumentation = new TypeDocumentation(source, typeof(TestClass), node);
 
         var propertyDocumentation = new PropertyDocumentation(source, propertyInfo, node);
         typeDocumentation.AddMemberData(propertyInfo, propertyDocumentation);
@@ -33,22 +31,21 @@ public class TypeDocumentationTests
         Assert.Same(propertyDocumentation, resolvedPropertyDocumentation);
         Assert.NotNull(resolvedPropertyDocumentation);
     }
-    
+
     [Fact]
     public void GetDocumentation_InheritedPropertyInfo_ReturnsPropertyDocumentation()
     {
         var source = new XDoc();
-        
+
         var xmlDocument = new XmlDocument();
         var node = xmlDocument.CreateTextNode("blah");
-        var inheritedPropertyInfo = typeof(InheritedTypeDocumentationTestClass).GetProperty(nameof(InheritedTypeDocumentationTestClass.MyProperty))!;
-        var propertyInfo = typeof(TypeDocumentationTestClass).GetProperty(nameof(TypeDocumentationTestClass.MyProperty))!;
-        
-        var typeDocumentation = new TypeDocumentation(source, typeof(TypeDocumentationTestClass), node);
+        var inheritedPropertyInfo = typeof(InheritedTestClass).GetProperty(nameof(InheritedTestClass.MyProperty))!;
+        var propertyInfo = typeof(TestClass).GetProperty(nameof(TestClass.MyProperty))!;
 
+        var propertyDocumentation = new PropertyDocumentation(source, propertyInfo, node);
         var inheritedPropertyDocumentation = new PropertyDocumentation(source, inheritedPropertyInfo, node);
-        var propertyDocumentation  = new PropertyDocumentation(source, propertyInfo, node);;
-        
+
+        var typeDocumentation = new TypeDocumentation(source, typeof(TestClass), node);
         typeDocumentation.AddMemberData(inheritedPropertyInfo, inheritedPropertyDocumentation);
         typeDocumentation.AddMemberData(propertyInfo, propertyDocumentation);
 
@@ -56,5 +53,27 @@ public class TypeDocumentationTests
 
         Assert.Same(propertyDocumentation, resolvedPropertyDocumentation);
         Assert.NotNull(resolvedPropertyDocumentation);
+    }
+
+    [Fact]
+    public void GetDocumentation_InheritedPropertyInfo_ShouldThorException()
+    {
+        var source = new XDoc();
+
+        var xmlDocument = new XmlDocument();
+        var node = xmlDocument.CreateTextNode("blah");
+        var inheritedPropertyInfo = typeof(InheritedTestClass).GetProperty(nameof(InheritedTestClass.MyProperty))!;
+
+        var typeDocumentation = new TypeDocumentation(source, typeof(InheritedTestClass), node);
+
+        var inheritedPropertyDocumentation = new PropertyDocumentation(source, inheritedPropertyInfo, node);
+
+        typeDocumentation.AddMemberData(inheritedPropertyInfo, inheritedPropertyDocumentation);
+
+        Assert.ThrowsAny<Exception>(() =>
+        {
+            var resolvedPropertyDocumentation = typeDocumentation.GetDocumentation(inheritedPropertyInfo);
+        });
+
     }
 }
