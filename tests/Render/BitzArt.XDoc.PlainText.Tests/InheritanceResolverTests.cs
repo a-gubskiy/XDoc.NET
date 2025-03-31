24b6034e58282ba55b7be404f41d19779a69afbe
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-03-31 18:57:08 +0300

    Update tests

--- tests/Render/BitzArt.XDoc.PlainText.Tests/InheritanceResolverTests.cs
+++ tests/Render/BitzArt.XDoc.PlainText.Tests/InheritanceResolverTests.cs
@@ -2,21 +2,17 @@ using System.Xml;
 
 namespace BitzArt.XDoc.Tests;
 
-class MyBaseClass
+abstract class MyBaseClass
 {
     /// <summary>
     /// My method in MyBaseClass
     /// </summary>
-    public virtual void MyMethod()
-    {
-    }
+    public abstract string MyMethod();
 }
 
 class MyClassA : MyBaseClass
 {
-    public override void MyMethod()
-    {
-    }
+    public override string MyMethod() => "MyClassA.MyMethod";
 }
 
 interface IMyInterface
@@ -24,148 +20,95 @@ interface IMyInterface
     /// <summary>
     /// My method in IMyInterface
     /// </summary>
-    void MyMethod();
+    string MyMethod();
 }
 
 class MyClassB : IMyInterface
 {
-    public void MyMethod()
-    {
-    }
+    public string MyMethod() => "MyClassB.MyMethod";
 }
 
 interface IMyInterface1 : IMyInterface
 {
-    void MethodA();
+    int MethodA();
 }
 
 interface IMyInterface2
 {
-    void MethodB();
+    int MethodB();
 }
 
 class MyClassC : IMyInterface1, IMyInterface2
 {
-    public void MyMethod()
-    {
-    }
+    public string MyMethod() => "MyClassC.MyMethod";
 
-    public void MethodA()
-    {
-    }
+    public int MethodA() => 0;
 
-    public void MethodB()
-    {
-    }
+    public int MethodB() => 0;
 }
 
 class MyClassD : MyClassA
 {
-    
 }
 
 public class InheritanceResolverTests
 {
+    private const string MyMethodInIMyInterface = "My method in IMyInterface";
+    private const string MyMethodInMyBaseClass = "My method in MyBaseClass";
+
     [Fact]
     public void GetTargetMember_CommentOnMethodInBaseClass_ShouldReturnComment()
     {
         XmlNode? node = null;
         var xdoc = new XDoc();
         var methodInfo = typeof(MyClassA).GetMethod(nameof(MyClassA.MyMethod))!;
-        
+
         var targetMember = InheritanceResolver.GetTargetMember(methodInfo, node);
         var documentationElement = xdoc.Get(targetMember!);
-        
+
         Assert.NotNull(documentationElement);
-        Assert.Equal("My method in MyBaseClass", documentationElement.Text);
+        Assert.Equal(MyMethodInMyBaseClass, documentationElement.Text);
     }
-    
+
     [Fact]
     public void GetTargetMember_CommentOnMethodInBaseClassOfBaseClass_ShouldReturnComment()
     {
         XmlNode? node = null;
         var xdoc = new XDoc();
         var methodInfo = typeof(MyClassD).GetMethod(nameof(MyClassD.MyMethod))!;
-        
+
         var targetMember = InheritanceResolver.GetTargetMember(methodInfo, node);
         var documentationElement = xdoc.Get(targetMember!);
-        
+
         Assert.NotNull(documentationElement);
-        Assert.Equal("My method in MyBaseClass", documentationElement.Text);
+        Assert.Equal(MyMethodInMyBaseClass, documentationElement.Text);
     }
-    
+
     [Fact]
     public void GetTargetMember_CommentOnMethodInBaseInterface_ShouldReturnComment()
     {
         XmlNode? node = null;
         var xdoc = new XDoc();
         var methodInfo = typeof(MyClassB).GetMethod(nameof(MyClassB.MyMethod))!;
-        
+
         var targetMember = InheritanceResolver.GetTargetMember(methodInfo, node);
         var documentationElement = xdoc.Get(targetMember!);
-        
+
         Assert.NotNull(documentationElement);
-        Assert.Equal("My method in IMyInterface", documentationElement.Text);
+        Assert.Equal(MyMethodInIMyInterface, documentationElement.Text);
     }
-    
+
     [Fact]
     public void GetTargetMember_CommentOnMethodInBaseInterfaceOfBaceInterace_ShouldReturnComment()
     {
         XmlNode? node = null;
         var xdoc = new XDoc();
         var methodInfo = typeof(MyClassC).GetMethod(nameof(MyClassC.MyMethod))!;
-        
+
         var targetMember = InheritanceResolver.GetTargetMember(methodInfo, node);
         var documentationElement = xdoc.Get(targetMember!);
-        
+
         Assert.NotNull(documentationElement);
-        Assert.Equal("My method in IMyInterface", documentationElement.Text);
+        Assert.Equal(MyMethodInIMyInterface, documentationElement.Text);
     }
-    
-    // [Fact]
-    // public void GetTargetMember_CommentOnMethodInParentClass_ShouldReturnComment()
-    // {
-    //     XmlNode? node = null;
-    //     var xdoc = new XDoc();
-    //     var type = typeof(TestingClass);
-    //     var memberInfo = type.GetMember(nameof(TestingClass.MethodThree)).First();
-    //
-    //     // Resolve the target member from the given member info.
-    //     var targetMember = InheritanceResolver.GetTargetMember(memberInfo, node);
-    //     var documentationElement = xdoc.Get(targetMember);
-    //
-    //     Assert.NotNull(documentationElement);
-    //     Assert.Equal("This is third method", documentationElement.Node.InnerText.Trim());
-    // }
-    //
-    //
-    // [Fact]
-    // public void GetTargetMember_CommentOnMethodInParentInterface_ShouldReturnComment()
-    // {
-    //     XmlNode? node = null;
-    //     var xdoc = new XDoc();
-    //     var type = typeof(TestingClass);
-    //     var memberInfo = type.GetMember(nameof(TestingClass.MethodOne)).First();
-    //     
-    //     var targetMember = InheritanceResolver.GetTargetMember(memberInfo, node);
-    //     var documentationElement = xdoc.Get(targetMember);
-    //
-    //     Assert.NotNull(documentationElement);
-    //     Assert.Equal("This is a method", documentationElement.Node.InnerText.Trim());
-    // }
-    //
-    // [Fact]
-    // public void GetTargetMember_CommentOnParentInterface_ShouldReturnComment()
-    // {
-    //     XmlNode? node = null;
-    //     var xdoc = new XDoc();
-    //     var type = typeof(TestingClass);
-    //     // var inheritdoc = CreateInheritdocElement();
-    //
-    //     var targetMember = InheritanceResolver.GetTargetMember(type, node);
-    //     var documentationElement = xdoc.Get(targetMember);
-    //
-    //     Assert.NotNull(documentationElement);
-    //     Assert.Equal("Test comment for the type", documentationElement.Node.InnerText.Trim());
-    // }
 }
