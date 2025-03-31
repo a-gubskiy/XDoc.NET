d0f24891301ff5e5e74124f66235c99e48828508
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-03-31 20:47:23 +0300

    cleanup

--- tests/Render/BitzArt.XDoc.PlainText.Tests/TestPropertyDocumentation.cs
+++ tests/Render/BitzArt.XDoc.PlainText.Tests/TestPropertyDocumentation.cs
@@ -3,22 +3,6 @@ using Moq;
 
 namespace BitzArt.XDoc.Tests;
 
-public class TestPropertyDocumentation : DocumentationElement
-{
-    public TestPropertyDocumentation(string xml)
-        : base(new XDoc(), GetXmlNode(xml))
-    {
-    }
-
-    private static XmlNode GetXmlNode(string xml)
-    {
-        var xmlDoc = new XmlDocument();
-        xmlDoc.LoadXml(@"<?xml version=""1.0"" encoding=""UTF-8"" standalone=""no""?>" + xml);
-
-        return xmlDoc.DocumentElement!; // Get the node
-    }
-}
-
 public class FakeMemberDocumentation : DocumentationElement
 {
     public FakeMemberDocumentation(XmlElement element)
