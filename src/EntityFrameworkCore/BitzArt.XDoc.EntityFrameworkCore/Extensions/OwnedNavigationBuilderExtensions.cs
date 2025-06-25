a22f966aec53f66768b42a622ffafe9c3628de19
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-06-25 14:27:34 +0300

    Implement OwnedNavigationBuilderExtensions

--- src/EntityFrameworkCore/BitzArt.XDoc.EntityFrameworkCore/Extensions/OwnedNavigationBuilderExtensions.cs
+++ src/EntityFrameworkCore/BitzArt.XDoc.EntityFrameworkCore/Extensions/OwnedNavigationBuilderExtensions.cs
@@ -2,13 +2,36 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BitzArt.XDoc;
 
+/// <summary>
+/// Provides extension methods for <see cref="OwnedNavigationBuilder{TOwnerEntity, TDependentEntity}"/> to map XML comments.
+/// </summary>
 public static class OwnedNavigationBuilderExtensions
 {
+    /// <summary>
+    /// Maps XML comments from an XDoc instance to an entity type and its properties.
+    /// </summary>
+    /// <typeparam name="TOwnerEntity">The type of the owner entity.</typeparam>
+    /// <typeparam name="TDependentEntity">The type of the dependent entity.</typeparam>
+    /// <param name="builder">The owned navigation builder to extend.</param>
+    /// <param name="xDoc">The XDoc instance containing XML comments to be mapped.</param>
+    /// <returns>The same builder instance so that multiple calls can be chained.</returns>
     public static OwnedNavigationBuilder<TOwnerEntity, TDependentEntity> MapXmlComments<TOwnerEntity, TDependentEntity>(
-        this OwnedNavigationBuilder<TOwnerEntity, TDependentEntity> builder)
+        this OwnedNavigationBuilder<TOwnerEntity, TDependentEntity> builder, XDoc xDoc)
         where TOwnerEntity : class
         where TDependentEntity : class
     {
-        throw new NotImplementedException();
+        var entityType = builder.Metadata.DeclaringEntityType;
+
+        entityType.ConfigureEntityTypeComment(xDoc);
+
+        var properties = entityType.GetProperties();
+
+        foreach (var property in properties)
+        {
+            entityType.ConfigureEntityPropertyComment(xDoc, property);
+        }
+
+        return builder;
     }
+
 }
