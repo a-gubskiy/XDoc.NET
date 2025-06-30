bcfce8f2331a0c9000c7261449f6d6f697539c69
Andrew Gubskiy <3822922+a-gubskiy@users.noreply.github.com>
2025-06-30 15:00:34 +0300

    Extend EF API (#54)

--- src/EntityFrameworkCore/BitzArt.XDoc.EntityFrameworkCore/Extensions/ModelBuilderExtensions.cs
+++ src/EntityFrameworkCore/BitzArt.XDoc.EntityFrameworkCore/Extensions/ModelBuilderExtensions.cs
@@ -1,6 +1,5 @@
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace BitzArt.XDoc;
 
@@ -21,76 +20,9 @@ public static class ModelBuilderExtensions
 
         foreach (var entityType in entityTypes)
         {
-            ConfigureEntity(xDoc, entityType);
+            entityType.ConfigureEntityComments(xDoc);
         }
 
         return modelBuilder;
     }
-
-    private static void ConfigureEntity(XDoc xDoc, IMutableEntityType entityType)
-    {
-        ConfigureEntityTypeComment(xDoc, entityType);
-
-        var properties = entityType.GetProperties();
-
-        foreach (var property in properties)
-        {
-            ConfigureEntityPropertyComment(xDoc, entityType, property);
-        }
-    }
-
-    private static void ConfigureEntityTypeComment(XDoc xDoc, IMutableEntityType entityType)
-    {
-        var typeDocumentation = xDoc.Get(entityType.ClrType);
-
-        if (typeDocumentation is null) 
-        {
-            // No own xml-documentation
-            return;
-        }
-        
-        var isOwned = entityType.IsOwned();
-        var tableName = entityType.GetTableName();
-
-        if (isOwned || tableName is null)
-        {
-            // For owned entities, we don't set the comment on the entity itself
-            // But we will set the comment on the properties
-            
-            return;
-        }
-        
-        var entityComment = typeDocumentation.ToPlainText();
-            
-        entityType.SetComment(entityComment);
-    }
-    
-    private static void ConfigureEntityPropertyComment(XDoc xDoc, IMutableEntityType entityType, IMutableProperty property)
-    {
-        var isShadowProperty = property.IsShadowProperty();
-
-        if (isShadowProperty)
-        {
-            return;
-        }
-
-        var propertyInfo = entityType.ClrType.GetProperty(property.Name);
-
-        if (propertyInfo is null)
-        {
-            return;
-        }
-
-        var propertyDocumentation = xDoc.Get(propertyInfo);
-
-        if (propertyDocumentation is null)
-        {
-            return;
-        }
-
-        var propertyComment = propertyDocumentation.ToPlainText();
-
-        property.SetComment(propertyComment);
-    }
-    
 }
