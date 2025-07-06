09b166fb5ed091790b41836c7b966cc3a78f0699
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-07-06 13:38:56 +0300

    Enhance XML documentation configuration for owned navigation builders

--- src/EntityFrameworkCore/BitzArt.XDoc.EntityFrameworkCore/Extensions/OwnedNavigationBuilderExtensions.cs
+++ src/EntityFrameworkCore/BitzArt.XDoc.EntityFrameworkCore/Extensions/OwnedNavigationBuilderExtensions.cs
@@ -8,21 +8,43 @@ namespace BitzArt.XDoc;
 public static class OwnedNavigationBuilderExtensions
 {
     /// <summary>
-    /// Maps XML comments from an XDoc instance to an entity type and its properties.
+    /// Configures XML documentation for the entity type represented by the owned navigation builder.
     /// </summary>
     /// <typeparam name="TOwnerEntity">The type of the owner entity.</typeparam>
     /// <typeparam name="TDependentEntity">The type of the dependent entity.</typeparam>
-    /// <param name="builder">The owned navigation builder to extend.</param>
-    /// <param name="xdoc">The XDoc instance containing XML comments to be mapped.</param>
-    /// <returns>The same builder instance so that multiple calls can be chained.</returns>
-    public static OwnedNavigationBuilder<TOwnerEntity, TDependentEntity> MapXmlComments<TOwnerEntity, TDependentEntity>(
-        this OwnedNavigationBuilder<TOwnerEntity, TDependentEntity> builder, XDoc xdoc)
+    /// <param name="builder">The owned navigation builder to configure.</param>
+    /// <param name="xdoc">The XML documentation container.</param>
+    /// <returns>The same builder instance so that multiple configuration calls can be chained.</returns>
+    public static OwnedNavigationBuilder<TOwnerEntity, TDependentEntity> ConfigureEntityXmlDocumentation<TOwnerEntity,
+        TDependentEntity>(
+        this OwnedNavigationBuilder<TOwnerEntity, TDependentEntity> builder,
+        XDoc xdoc)
         where TOwnerEntity : class
         where TDependentEntity : class
     {
         var entityType = builder.Metadata.DeclaringEntityType;
 
         entityType.ConfigureEntityTypeComment(xdoc);
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Configures XML documentation for all properties of the entity type represented by the owned navigation builder.
+    /// </summary>
+    /// <typeparam name="TOwnerEntity">The type of the owner entity.</typeparam>
+    /// <typeparam name="TDependentEntity">The type of the dependent entity.</typeparam>
+    /// <param name="builder">The owned navigation builder to configure.</param>
+    /// <param name="xdoc">The XML documentation container.</param>
+    /// <returns>The same builder instance so that multiple configuration calls can be chained.</returns>
+    public static OwnedNavigationBuilder<TOwnerEntity, TDependentEntity> ConfigurePropertiesXmlDocumentation<
+        TOwnerEntity, TDependentEntity>(
+        this OwnedNavigationBuilder<TOwnerEntity, TDependentEntity> builder,
+        XDoc xdoc)
+        where TOwnerEntity : class
+        where TDependentEntity : class
+    {
+        var entityType = builder.Metadata.DeclaringEntityType;
 
         var properties = entityType.GetProperties();
 
@@ -33,5 +55,4 @@ public static class OwnedNavigationBuilderExtensions
 
         return builder;
     }
-
 }
